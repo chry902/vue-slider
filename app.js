eb647bfcb37bcd182e78166856f4dd8e0ac85381
chry902 <tchry902@gmail.com>
2021-10-29 15:00:23 +0200

    stampato gli oggetti

---
+++ app.js
@@ -0,0 +1,47 @@
+/**Descrizione:
+Rifare l'esercizio dello slider come fatto assieme in classe.
+Bonus:
+Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente. */
+
+const app = new Vue({
+    el: "#app",
+    data:
+    {
+        slides: [
+            {
+                items: './img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                items: './img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                items: './img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                items: './img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                items: './img/05.jpg',
+                title: 'Paradise',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            }
+        ]
+    },
+    methods: {
+        slideUp() {
+
+        },
+        sludeDown() { }
+    }
+
+})
+
+
